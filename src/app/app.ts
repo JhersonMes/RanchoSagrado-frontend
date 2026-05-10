0edc972304fb3136002cde3efa4ce75347468bc0
JhersonMes <mishifug@gmail.com>
2026-05-10 10:02:12 +0200

    Feat: Se añadio las importaciones necesarias

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ClientComponent } from './pages/client/client.component';
+import { SupplierComponent } from './pages/supplier/supplier.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClientComponent, SupplierComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
