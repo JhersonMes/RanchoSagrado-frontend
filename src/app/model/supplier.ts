c81776912b0f8caaba44c6529b30be7af9c63be8
JhersonMes <mishifug@gmail.com>
2026-05-10 10:04:55 +0200

    Add: Se agrego modelo "Supplier"

---
+++ src/app/model/supplier.ts
@@ -0,0 +1,10 @@
+export class Supplier {
+    idSupplier: number;
+    phone: string;
+    ruc: string;
+    companyName: string;
+    contactEmail: string;
+    address: string;
+    supplyType: string;
+    status: string;
+}
