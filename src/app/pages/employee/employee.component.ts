5de6f39a33ee974ed98b807ac932ebfb8a509b1e
ANTHOX123 <huaripatabrayan61@gmail.com>
2026-06-19 00:52:36 -0500

    "Implementa notificaciones y limpieza de mensajes con SnackBar"

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -34,7 +34,7 @@ export class EmployeeComponent {
   protected displayedColumns = ['idEmployee', 'name', 'lastName', 'dni', 'job', 'phone', 'status', 'actions'];
 
   constructor() {
-    this.service.findAll().subscribe(data => this.service.setListChange(data));
+    this.service.findAll().subscribe(data => this.service.setListChange(data));// Muestra un mensaje informativo y limpia el estado del mensaje después de notificar al usuario.
 
     effect(() => {
       const ds = this.$dataSource();
